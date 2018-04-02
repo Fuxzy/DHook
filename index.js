9821354e2d5194f22f8fce4c17b4a14b65619ad7
Fuxzy <34891539+Fuxzy@users.noreply.github.com>
2018-04-02 10:41:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 TemmieWebhook temmie = new TemmieWebhook("https://discordapp.com/api/webhooks/405871606729146369/eNbBJ5fUxqUAzLJ8lJHXMYei53fJ8Tahs6_y6MA4XEO2Vn1Wuz9S73nCsEvH10-PS4rR");
-DiscordMessage dm = new DiscordMessage("Test", "Test", "null");
+DiscordMessage dm = new DiscordMessage("Test", "Test", "null")
